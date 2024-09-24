7c5dea8633bbc1bd379488eb4fd96b3aa3b2e787
Bang_sv <zahar.yury@mail.ru>
2024-09-24 16:49:06 +0300

    fix_backend

--- backend/quiz/asgi.py
+++ backend/quiz/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backend project.
+ASGI config for quiz project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz.settings')
 
 application = get_asgi_application()
