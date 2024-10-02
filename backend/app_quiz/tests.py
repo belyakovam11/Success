520b52e1a0d499b89e647f6e0e44161c903afab7
nastikver <nastikver1@yandex.ru>
2024-10-02 14:15:15 +0300

    Обновление backend
    Новый файл celery_app.py, изменение в файлах requirments.txt.,tests.py, settings.py для подключения celery,rabbitMQ и redis

--- backend/app_quiz/tests.py
+++ backend/app_quiz/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from celery import shared_task
 
 class SimpleTest(TestCase):
     def test(self):
@@ -8,3 +9,7 @@ class SimpleTest(TestCase):
     def test2(self):
         a = 5
         self.assertEqual(a, 5)  # Это приведет к ошибке, т.к. a не равно 2
+
+@shared_task
+def add(x, y):
+    return x + y
--- backend/quiz/settings.py
+++ backend/quiz/settings.py
@@ -121,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
