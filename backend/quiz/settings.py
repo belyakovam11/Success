ea247e150cce14cacb5fb20309ce2461cfe46728
Bang_sv <zahar.yury@mail.ru>
2024-10-17 17:35:20 +0300

    create_setting_py

--- backend/quiz/settings.py
+++ backend/quiz/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'quiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',  # Имя базы данных
+        'USER': 'django',  # Пользователь
+        'PASSWORD': 'django',  # Пароль
+        'HOST': 'db',  # Название сервиса в Docker Compose (имя контейнера)
+        'PORT': '5432',  # Порт для PostgreSQL
     }
 }
 
