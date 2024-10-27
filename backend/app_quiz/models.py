9f14c69dbab672e2efeb2c52f9a984c441153542
belyakovam11 <belyakova110603@gmail.com>
2024-10-27 20:41:17 +0300

    Update models.py

--- backend/app_quiz/models.py
+++ backend/app_quiz/models.py
@@ -1,3 +1,16 @@
+# app_quiz/models.py
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Добавьте дополнительные поля, если необходимо
+    pass
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
