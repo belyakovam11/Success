c1ee3c8b674c1bf2a336913e392a1688e793f582
Bang_sv <zahar.yury@mail.ru>
2024-10-28 03:51:00 +0300

    fix_csrf

--- backend/app_quiz/views.py
+++ backend/app_quiz/views.py
@@ -9,6 +9,11 @@ from django.db import connection
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import authenticate, login
 import json
+from django.utils.deprecation import MiddlewareMixin
+
+class DisableCSRFMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        setattr(request, '_dont_enforce_csrf_checks', True)
 
 def get_time(request):
     data = {
