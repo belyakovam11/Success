f98c8c3bb33a8f14919b9650742a57194149100f
nastikver <nastikver1@yandex.ru>
2024-11-28 00:02:32 +0300

    update __init__.py

--- backend/user/__init__.py
+++ backend/user/__init__.py
@@ -4,19 +4,19 @@ import requests
 
 session = requests.Session() 
 
-def get_time(request):
+def get_time(request): #Обрабатывает GET-запрос и возвращает информацию о пользователе из сессии
     if request.method == 'GET':
         session_id = session.cookies.get('sessionid')  # Получаем sessionid из cookies сессии requests
         print("Session ID:", session_id)
-        sys.stdout.flush()  # Сброс вывода
+        sys.stdout.flush()   # Сбрасываем вывод, чтобы убедиться, что данные сразу отображаются
 
         username = session.cookies.get('username')  # Получаем имя из cookies сессии requests
         print("Username in session:", username)
         sys.stdout.flush()  # Сброс вывода
 
         if username:
-            return JsonResponse({"username": username}, status=200)
+            return JsonResponse({"username": username}, status=200) 
         else:
-            return JsonResponse({"error": "Username not found in session."}, status=404)
+            return JsonResponse({"error": "Username not found in session."}, status=404) # Возвращаем ошибку, если имя пользователя не найдено
 
-    return JsonResponse({"error": "Invalid request method."}, status=400)
+    return JsonResponse({"error": "Invalid request method."}, status=400) # Возвращаем ошибку, если метод запроса не GET
