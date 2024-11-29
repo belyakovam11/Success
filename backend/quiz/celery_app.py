91b61553e26bbbd0a33378597692134c9149a984
nastikver <nastikver1@yandex.ru>
2024-11-29 20:48:18 +0300

    update celery_app.py

--- backend/quiz/celery_app.py
+++ backend/quiz/celery_app.py
@@ -2,9 +2,16 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
+# Устанавливаем переменную окружения для Django, чтобы указать, какой файл настроек использовать
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz.settings')
+
+# Создаем объект Celery с именем 'quiz'
 app = Celery('quiz')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Загружаем конфигурацию Celery из настроек Django
+app.config_from_object('django.conf:settings', namespace='CELERY') 
+
+# Автоматически обнаруживаем задачи (tasks) из приложений
 app.autodiscover_tasks()
 
 
